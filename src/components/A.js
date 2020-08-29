4d735b462d9ae0da868a201559081a3e1d1583b2
satyamkalra143 <satyamkalra0@gmail.com>
2020-08-29 07:58:51 +0530

    InitialS commit

--- src/components/A.js
+++ src/components/A.js
@@ -20,31 +20,75 @@ class Deploy extends Component {
     // console.log(this.state)
     const classes = this.props.classes
     return (
-      <div >
+      <div>
           {/* <B/> */}
               <Grid container>
+
+              <Grid xs={12} md={12} lg={12} className={classes.grid}>
+                <h1> Introduce Yourself </h1>
+                </Grid>
+
                 <Grid xs={12} md={12} lg={12} className={classes.grid}>
-                <TextField id="standard-basic" onChange={this.props.F}  label="First Name" />
+                <TextField id="standard-full-width"
+                style={{ width:450  }}
+                placeholder="First Name"
+                //helperText="Full width!"
+                required="true"
+                margin="normal" onChange={this.props.F}  label="First Name" />
 
                 </Grid>
                 <Grid xs={12} md={12} lg={12} className={classes.grid}>
-            <TextField id="standard-basic" label="Last Name" />
+            <TextField id="standard-full-width"
+               label="Last Name"
+               style = {{width: 450}} 
+                placeholder="Last Name"
+                //helperText="Full width!"
+                fullWidth="false"
+                margin="normal"  />
 
                 </Grid>
                 <Grid xs={12} md={12} lg={12} className={classes.grid}>
-                <TextField id="standard-basic" label="Phone" />
+                <TextField 
+                id="standard-full-width"
+                label="Phone"
+                style = {{width: 450}} 
+                placeholder="Phone"
+                //helperText="Full width!"
+                //fullWidth
+                margin="normal"  />
 
                 </Grid>
                 <Grid xs={12} md={12} lg={12} className={classes.grid}>
-            <TextField id="standard-basic" label="Email" />
+            <TextField id="standard-full-width"
+                label="Email"
+                style = {{width: 450}} 
+                placeholder="Email"
+                //helperText="Full width!"
+                //fullWidth
+                margin="normal"  />
 
                 </Grid>
                 <Grid xs={12} md={12} lg={12} className={classes.grid}>
-            <TextField id="standard-basic" label="Address" />
+            <TextField id="standard-full-width"
+                label="Address"
+                style = {{width: 450}} 
+                placeholder="Address"
+                //helperText="Full width!"
+                //fullWidth
+                margin="normal"  />
 
                 </Grid>
               </Grid>
-                <button onClick={this.props.continue} >continue</button>
+            
+                <Grid container justify="center"> 
+                  <Grid item>
+          
+         
+          
+                   <Button variant="contained" color="primary" style = {{margin: 50}}  disableElevation="false" onClick={this.props.continue}>Continue</Button>
+          
+                  </Grid>
+                </Grid>
 
       </div>
     )
@@ -56,8 +100,13 @@ const styles = theme => ({
   grid:{
       display:'flex',
       justifyContent:'center',
-      alignItems:'center'
+      alignItems:'center',
+      width:'100vh'
   }
+
+  
+    
+    
   
 })
 export default withStyles(styles)(Deploy);
